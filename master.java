f11e3758c733935f4ea99a4de05bbd7bd5eeb65d
Rushikesh Chavan <rushikeshchavan@Rushikeshs-MacBook-Air-2.local>
2025-07-11 01:59:48 +0530

    some leetcode problems

--- master.java
+++ master.java
@@ -44,7 +44,7 @@ public class master {
 
                 int left_up = matrix[i][j];
                 if (j > 0) {
-                    left_up +=  dp[i - 1][j + 1];
+                    left_up += dp[i - 1][j + 1];
                 } else {
                     left_up = (int) Math.pow(-10, 9);
                 }
@@ -85,82 +85,165 @@ public class master {
         return maxi;
     }
 
-
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //now, solving the first 3d dp proble,, which is chocolate puckup by two starting positions 
-    public static int chocolatepickupUtil(int i, int j1, int j2,int n,  int m, int[][] matrix, int[][][] dp){
-        //base cases 
-        if(i == n - 1 ) {
-            if(j1 == j2) {
+    // now, solving the first 3d dp proble,, which is chocolate puckup by two
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// starting
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// positions
+    public static int chocolatepickupUtil(int i, int j1, int j2, int n, int m, int[][] matrix, int[][][] dp) {
+        // base cases
+        if (i == n - 1) {
+            if (j1 == j2) {
                 return matrix[i][j1];
-            }else {
+            } else {
                 return matrix[i][j1] + matrix[i][j2];
             }
         }
-        //writing edge cases
-        if(j1 < 0 || j2 >=m || j2 < 0 || j1 >=m) {
+        // writing edge cases
+        if (j1 < 0 || j2 >= m || j2 < 0 || j1 >= m) {
             return (int) Math.pow(-10, 9);
         }
         int maxi = 0;
-        //exploring all paths
-        if(dp[i][j1][j2] != -1) {
+        // exploring all paths
+        if (dp[i][j1][j2] != -1) {
             return dp[i][j1][j2];
         }
-        for(int j = -1; j <=1; j++) {
-            for(int k = -1; k <=1; k++) {
-                if(j1 == j2) {
-                    maxi = Math.max(maxi, matrix[i][j1] + chocolatepickupUtil(i+1,j1 + j, j2 + k, n, m, matrix, dp));
-                }else {
-                    maxi = Math.max(maxi, matrix[i][j1] + matrix[i][j2] + chocolatepickupUtil(i+1,j1 + j, j2 + k, n, m, matrix, dp));
+        for (int j = -1; j <= 1; j++) {
+            for (int k = -1; k <= 1; k++) {
+                if (j1 == j2) {
+                    maxi = Math.max(maxi, matrix[i][j1] + chocolatepickupUtil(i + 1, j1 + j, j2 + k, n, m, matrix, dp));
+                } else {
+                    maxi = Math.max(maxi, matrix[i][j1] + matrix[i][j2]
+                            + chocolatepickupUtil(i + 1, j1 + j, j2 + k, n, m, matrix, dp));
                 }
             }
         }
         return dp[i][j1][j2] = maxi;
     }
-    public static int chocolatepickupTabulation(int[][] matrix){
-        //creating the dp
+
+    public static int chocolatepickupTabulation(int[][] matrix) {
+        // creating the dp
         int n = matrix.length;
         int m = matrix[0].length;
         int[][][] dp = new int[n][m][m];
-        //writing the base cases 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < m; j++) {
-                if(i == j) {
-                    dp[n-1][i][j] = matrix[n-1][j];
-                }else {
-                    dp[n-1][i][j] = matrix[n-1][i] + matrix[n-1][j];
+        // writing the base cases
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == j) {
+                    dp[n - 1][i][j] = matrix[n - 1][j];
+                } else {
+                    dp[n - 1][i][j] = matrix[n - 1][i] + matrix[n - 1][j];
                 }
             }
         }
-        //declaring the maxi variable
-        int maxi = 0;
-        //iterating the loops. Condition 2
-        for(int i = n-1; i >=0; i++) {
-            for(int j1 = -1; j1 <= +1; j1++) {
-                for(int j2 = -1; j2 <= +1 ; j2++) {
-                    if(j1 == j2) {
-                    maxi = Math.max(maxi, matrix[i][j1] + dp[i-1][j1][j1]);
-                }else {
-                    maxi = Math.max(maxi, matrix[i][j1] + matrix[i][j2] + dp[i-1][j1][j2]);
+        // declaring the maxi variable
+        // int maxi = 0;
+        // iterating the loops. Condition 2
+        for (int i = n - 1; i >= 0; i++) {
+            for (int k = 0; k < m; k++) {
+                for (int l = 0; l < m; l++) {
+                    int maxi = 0;
+                    for (int j1 = -1; j1 <= +1; j1++) {
+                        for (int j2 = -1; j2 <= +1; j2++) {
+                            int ans;
+                            if (k == l) {
+                                ans = matrix[i][k];
+                                // ans = Math.max(maxi, matrix[i][k] + dp[i + 1][j1][j1]);
+                            } else {
+                                ans = matrix[i][k] + matrix[i][l];
+                                // ans = Math.max(maxi, matrix[i][k] + matrix[i][j2] + dp[i + 1][j1][j2]);
+                            }
+                            // Check if the indices are valid
+                            if ((k + j1 < 0 || k + j1 >= m) || (l + j2 < 0 || l + j2 >= m))
+                                ans += (int) Math.pow(-10, 9);
+                            else
+                                ans += dp[i + 1][k + j1][l + j2];
+                            // Update maxi with the maximum result
+                            maxi = Math.max(ans, maxi);
+
+                        }
+                    }
+                    dp[i][k][l] = maxi;
                 }
-                dp[i][j1][j2] = maxi;
-                }  
             }
         }
-        return dp[0][0][m-1];
+        return dp[0][0][m - 1];
     }
 
     public static int chocolatepickup(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
         int[][][] dp = new int[n][m][m];
-        for(int[][] i : dp) {
-            for(int[] j: i){
+        for (int[][] i : dp) {
+            for (int[] j : i) {
                 Arrays.fill(j, -1);
             }
         }
 
-        return chocolatepickupUtil(0, 0, m-1, n, m, matrix, dp);
+        return chocolatepickupUtil(0, 0, m - 1, n, m, matrix, dp);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // now we shall solve the famous problems on dp on stocks
+    public static int maximumProfit(int[] arr) {
+        int min = arr[0];
+        int profit = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int cost = arr[i] = min;
+            profit = Math.max(profit, cost);
+            min = Math.min(min, arr[i]);
+        }
+
+        return profit;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // now, gponnna attemp teh problem of MCM(Matrix Chain Multiplication)
+    // first gonna write the recursion, then the multiplication
+    public static int MatrixMultiplicationUtil(int i, int j, int[] arr, int[][] dp) {
+        // writing the base case
+        if (i == j) {
+            return 0;
+        }
+        // writing the recurive cases
+        // we simply have to iterate and give the miimum
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int mini = Integer.MIN_VALUE;
+        for (int k = i; k <= j - 1; k++) {
+            int ans = arr[i - 1] * arr[k] * arr[j] + MatrixMultiplicationUtil(i, k, arr, dp);
+            mini = Math.min(ans, mini);
+        }
+
+        return dp[i][j] =  mini;
+    }
+
+
+    public static int MatrixMultplicationTabulation(int[] arr, int N) {
+
+
+        int[][] dp = new int[N][N];
+        for (int[] o : dp) {
+            Arrays.fill(dp, -1);
+        }
+        //writing the base case first
+        for(int i = 0; i < N; i++) {
+            dp[i][i] = 0;
+        }
+
+        //now, loping what was changing in the memoiation approach 
+        for(int i = ) {}
+    }
+
+    public static int MatrixMultiplication(int[] arr, int N) {
+        int i = 1;
+        int j = N - 1;
+        int[][] dp = new int[N][N];
+        for (int[] o : dp) {
+            Arrays.fill(dp, -1);
+        }
+        return MatrixMultiplicationUtil(i, j, arr, dp);
     }
 
     public static void main(String[] args) {
---
+++ maxprofits.java
@@ -0,0 +1,18 @@
+public class maxprofits {
+
+        public static int maximumProfit(int[] arr) {
+        int min = arr[0];
+        int profit = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            int cost = arr[i] = min;
+            profit = Math.max(profit, cost);
+            min = Math.min(min, arr[i]);
+        }
+
+        return profit;
+    }
+
+    
+    
+}
